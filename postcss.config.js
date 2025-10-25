9958896ee2790d7969563b6a65919ee827a6a731
mon664 <Hhtsta@gmail.com>
2025-10-25 13:25:11 +0900

    fix: remove duplicates and update Tailwind v4 postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// postcss.config.js 
+module.exports = { 
+  plugins: { 
+    '@tailwindcss/postcss': {}, 
+  }, 
+}; 
